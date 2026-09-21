1240e3332bb45f050225fdea3dae366e3eae82c5
slv21042104-stack <slv21042104@gmail.com>
2026-09-21 19:25:05 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-from .config import settings
+from config import settings
 
 engine = create_async_engine(settings.database_url, echo=False, pool_pre_ping=True)
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
