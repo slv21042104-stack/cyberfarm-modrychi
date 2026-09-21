875286c41516559d6b8415011dc4424354694374
slv21042104-stack <slv21042104@gmail.com>
2026-09-21 21:27:39 +0300

    Update database.py

--- database.py
+++ database.py
@@ -13,6 +13,7 @@ async def get_db():
         yield session
 
 async def init_db():
-    from import models
+    import models
+
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
